1b643cd882067dc6d2d238f91d19e029e402760a
Roger Hall <rogerhall123@gmail.com>
2021-08-13 18:45:16 -0600

    6th commit

--- next.config.js
+++ next.config.js
@@ -4,3 +4,24 @@ module.exports = {
     reactStrictMode : true
 
 }
+
+// from https://vercel.com/support/articles/how-to-enable-cors:
+// " Enabling CORS in a Next.js App
+// The headers() function allows you to define paths associated with a set of headers. It
+// can be useful to allow CORS in multiple routes. "
+module.exports = {
+    async headers() {
+        return [
+            {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+            }
+        ]
+    }
+};
